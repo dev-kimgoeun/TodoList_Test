edcf8a87b6eabcda8fe3fefe74894a29d267100c
goeunkim <ak0150a01@gmail.com>
2024-01-17 13:57:50 +0900

    login

--- src/main/java/org/zerok/jdbcex/controller/TodoRegisterController.java
+++ src/main/java/org/zerok/jdbcex/controller/TodoRegisterController.java
@@ -8,6 +8,7 @@ import org.zerok.jdbcex.service.TodoService;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpSession;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
@@ -24,6 +25,21 @@ public class TodoRegisterController extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         log.info("/todo/register GET ");
+
+        HttpSession session = req.getSession();
+
+        if(session.isNew()) {
+            log.info("JSESSIONID ");
+            resp.sendRedirect("/login");
+            return;
+        }
+
+        if(session.getAttribute("loginInfo") ==null){
+            log.info("로그인한 정보가 없는 사용자");
+            resp.sendRedirect("/login");
+            return;
+        }
+
         req.getRequestDispatcher("/WEB-INF/todo/register.jsp").forward(req,resp);
     }
     @Override
