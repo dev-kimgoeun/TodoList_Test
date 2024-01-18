d6b7d85b202760bd584fa7327e85f907e3bbfbdc
goeunkim <ak0150a01@gmail.com>
2024-01-18 18:08:45 +0900

    uuid cookie

--- src/main/java/org/zerok/jdbcex/filter/LoginCheckFilter.java
+++ src/main/java/org/zerok/jdbcex/filter/LoginCheckFilter.java
@@ -1,13 +1,22 @@
 package org.zerok.jdbcex.filter;
 
 import lombok.extern.log4j.Log4j2;
+import org.zerok.jdbcex.dto.MemberDTO;
+import org.zerok.jdbcex.service.MemberService;
+import sun.security.ssl.CookieExtension;
 
 import javax.servlet.*;
 import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Optional;
+
+import static java.util.spi.ToolProvider.findFirst;
 
 @WebFilter(urlPatterns = {"/todo/*"})
 @Log4j2
@@ -21,10 +30,48 @@ public class LoginCheckFilter implements Filter {
         HttpServletResponse resp = (HttpServletResponse) response;
 
         HttpSession session = req.getSession();
+
+        if(session.getAttribute("loginInfo") != null){
+            chain.doFilter(request,response);
+            return;
+        }
+
+        Cookie cookie = findCookie(req.getCookies(), "remember-me");
+
         if(session.getAttribute("loginInfo") == null){
             resp.sendRedirect("/login");
             return;
         }
-        chain.doFilter(request, response);
+
+        log.info("cookie는 존재하는 상황");
+
+        String uuid = cookie.getValue();
+
+        try{
+            MemberDTO memberDTO = MemberService.INSTANCE.getByUUID(uuid);
+
+            log.info("쿠키의 값으로 조회한 사용자 정보 : " + memberDTO);
+            if(memberDTO == null){
+                throw new Exception("COOKIE VALUE IS NOT VALID");
+            }
+
+            session.setAttribute("loginInfo", memberDTO);
+            chain.doFilter(request, response);
+        } catch (Exception e) {
+            e.printStackTrace();
+            resp.sendRedirect("/login");
+        }
+    }
+
+    private Cookie findCookie(Cookie[] cookies, String name){
+        if(cookies == null || cookies.length ==0){
+            return null;
+        }
+
+        Optional<Cookie> result = Arrays.stream(cookies)
+                .filter(ck -> ck.getName().equals(name))
+                .findFirst();
+
+        return result.isPresent()?result.get():null;
     }
 }
